80c97195739f197ef6c7606272a6a728daa888e8
Riccardo Gulin <riccardo.gulin@gmail.com>
2026-01-09 13:00:13 +0100

    Adds Polymorphism example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import entities.Cat;
-import entities.Dog;
+import entities.*;
+import interfaces.Jumper;
+import interfaces.Runner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -8,18 +9,56 @@ public class Main {
 		// System.out.println(a);
 
 		Dog dog = new Dog("Rex", 2, true);
-		dog.sayYourName();
+		// dog.sayYourName();
 		//dog.sayYourName();
 //
-		System.out.println(dog);
+
 
 		Cat cat = new Cat("Felix", 3, true);
 //		cat.sayYourName("CIAO A TUTTI");
 
-		System.out.println(cat);
 
-		dog.makeSound();
-		cat.makeSound();
+		Student student = new Student("Aldo", "Baglio", true, StudentType.FULLSTACK); // è un RUNNER
+		Dog ringhio = new Dog("Ringhio", 3, false); // è un RUNNER
 
+		Runner[] runners = {student, ringhio};
+		Jumper[] jumpers = {student, cat};
+		Animal[] animals = {dog, cat, ringhio};
+
+		for (int i = 0; i < animals.length; i++) {
+			System.out.println("Valore i: " + i);
+			animals[i].makeSound();
+
+			// Se volessi utilizzare metodi specifici di un certo animale (cane o gatto)
+			// devo come prima cosa verificare se l'animale corrente tra i suoi tipi ha Cat (o Dog)
+			if (animals[i] instanceof Cat) {
+				// Prima di utilizzare il metodo specifico di Cat devo convertire l'Animal in Cat
+				Cat currentCat = (Cat) animals[i]; // Il CASTING ESPLICITO lo posso usare solo quando solo sicuro di poterlo fare
+				// in questo caso abbiamo prima controllato con instanceof se stiamo usando un gatto
+				currentCat.jump(10); // Qua posso usare un metodo specifico dei gatti e non dei cani
+			} else {
+				System.out.println(animals[i] + " non è un gatto");
+			}
+		}
+
+		for (Runner runner : runners) {
+			runner.run(10);
+		}
+
+		for (Jumper jumper : jumpers) {
+			jumper.jump(20);
+		}
+
+		makeAnimalSound(ringhio);
+		makeAnimalSound(cat);
 	}
+
+	public static void makeAnimalSound(Animal animal) { // Se il parametro lo dichiaro come Animal, sfrutto il polimorfismo, per passare
+		// sia cani che gatti a questo metodo, rendendolo molto più flessibile
+		animal.makeSound();
+	}
+
+//	public static void makeAnimalSound(Dog dog) { // Questo mi permette di passare SOLO ESCLUSIVAMENTE cani, quindi meno versatile
+//		dog.makeSound();
+//	}
 }
